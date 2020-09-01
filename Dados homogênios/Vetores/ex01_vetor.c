9b07bd1199969f15841a32927160d70901895e70
httpLucasReis <manoel.reisiema@gmail.com>
2020-09-01 19:46:46 -0300

    Exemplo 1: vetores

--- Dados homogênios/Vetores/ex01_vetor.c
+++ Dados homogênios/Vetores/ex01_vetor.c
@@ -1,12 +1,13 @@
 /*
-  Exemplo de algoritmo que utiliza um vetor para calcular
-  a médio de números.
+  Exemplo de um algoritmo que utiliza um vetor para calcular
+  a medio de numeros.
 */
 #include <stdlib.h>
 #include <stdio.h>
 #include <locale.h>
 #define SIZE 5
 
+// Funcao que soma todos os elementos do array
 int sumValues(int *array, int size){
   int sum = 0;
 
@@ -17,31 +18,37 @@ int sumValues(int *array, int size){
   return sum;
 }
 
+// Funï¿½ï¿½o que calcula a mï¿½dia;
 float caclAvarage(float total, int length) {
-  printf("%f\n", total/length);
   return total/length;
-
 }
 
 int main(void){
   setlocale(LC_ALL, "Portuguese");
 
   int value;
-  int array[SIZE];
-  int total;
-  float avarage;
+  int array[SIZE]; // array do tipo inteiro que um tamanho maximo de 5 elementos
+  int total;       // Variavel que recebe a soma de todos os elementos do array
+  float avarage;   // Variavel que recebe a media dos elementos do array
 
+  // Recebendo dados
   for(int i = 0; i < SIZE; i++){
     printf("Digite um valor: \n");
     scanf("%d",&value);
     array[i] = value;
   }
 
+  // Realizando operacoes
   total = sumValues(array, SIZE);
   avarage = caclAvarage(total, SIZE);
 
-  printf("A média é : %.2f\n", avarage);
+  // Exibindo o resultado
+  printf("A media eh : %.2f\n", avarage);
 
  system("pause");
  return 0;
 }
+
+/*
+  Created by Manoel Lucas Lopes Reis
+*/
